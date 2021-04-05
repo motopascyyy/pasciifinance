a792a7919d470fb8fb3b687e189e8d2cd38016c1
pasdeignan <pasdeignan@gmail.com>
2021-04-05 16:39:47 -0400

    0.0.4-SNAPSHOT big restructuring changes and new objects to be used for automated data addition to DB (AccountAllocationEntry*java) - added new AccountAllocationEntry entities and repos - split up project into multiple modules to ease the way for the SpringBatch jobs for fetching data from financial institutions - removed application-prod.properties because it will be manually maintained outside of git due to sensitive information being present - updated the version of gradle because why not - minor HTML and css changes for formatting on several different screen sizes - added automatic growth calculator field in HTML and JavaScript - Created the first REST endpoint for total_allocation. Not yet tested as I need to work on the automated data entry first - Started using SonarLint to improve code quality. The benefits have not yet been yielded yet.

---
+++ common/src/test/java/com/pasciitools/pasciifinance/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasciitools.pasciifinance.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
