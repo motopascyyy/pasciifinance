a51f418c6e3faf780e6e6b04e5ce2de4e39f5b86
motopascyyy <54141000+motopascyyy@users.noreply.github.com>
2021-04-15 08:53:30 -0400

    feature/transform_excel_loader_to_spring_batch: created new AccountService and added Repository annotations to Repo classes so that IDEA wouldn't complain

---
+++ common/src/main/java/com/pasciitools/pasciifinance/common/service/AccountService.java
@@ -0,0 +1,32 @@
+package com.pasciitools.pasciifinance.common.service;
+
+import com.pasciitools.pasciifinance.common.entity.Account;
+import com.pasciitools.pasciifinance.common.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+    @Autowired
+    private AccountRepository acctRepo;
+
+    public Account getAccount(String institutionName, String accountName) {
+        //TODO add some caching logic here for better performance
+        return acctRepo.findByInstitutionAndAccountLabel(institutionName, accountName);
+    }
+
+    public Account getAccountFromSheetName(String sheetName) {
+        String acctLabel = getAcctLabelFromSheetName(sheetName);
+        String inst = getInstitutionFromSheetName(sheetName);
+        return getAccount(inst, acctLabel);
+    }
+
+    public String getInstitutionFromSheetName (String sheetName) {
+        return sheetName.split(" ")[0].trim();
+    }
+
+    public String getAcctLabelFromSheetName (String sheetName) {
+        String inst = getInstitutionFromSheetName(sheetName);
+        return sheetName.substring((inst.length())).trim();
+    }
+}
